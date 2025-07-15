d6b305e3fb77da90d67938059d771fdb3f9fc46f
VuMinhQuangVN <vuminhquang567@gmail.com>
2025-07-15 14:43:58 +0700

    nhanhquang_lan2

---
+++ DiemRenLuyen/DiemRenLuyen/Controllers/TaiKhoansController.cs
@@ -0,0 +1,38 @@
+﻿using DiemRenLuyen.Data;
+using DiemRenLuyen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiemRenLuyen.Controllers
+{
+    public class TaiKhoansController : Controller
+    {
+        private readonly DiemRenLuyenContext _context;
+        public TaiKhoansController(DiemRenLuyenContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(TaiKhoan model)
+        {
+            var user = _context.TaiKhoan.FirstOrDefault(x => x.TenDangNhap == model.TenDangNhap && x.MatKhau == model.MatKhau);
+            if (user != null)
+            {
+                // Đăng nhập thành công → chuyển trang theo vai trò
+                if (user.VaiTro == "SV")
+                    return RedirectToAction("Index", "SinhViens");  // Chưa có controller này thì sẽ báo lỗi
+                else if (user.VaiTro == "GV")
+                    return RedirectToAction("Index", "GiangViens");
+                else if (user.VaiTro == "CTSV")
+                    return RedirectToAction("Index", "CTSVs");
+            }
+
+            ViewBag.ThongBao = "Đăng nhập thất bại. Kiểm tra lại tài khoản hoặc mật khẩu!";
+            return View("Index");
+        }
+    }
+}
--- DiemRenLuyen/DiemRenLuyen/Program.cs
+++ DiemRenLuyen/DiemRenLuyen/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=TaiKhoans}/{action=Index}/{id?}");
 
 app.Run();
